14bbe0214f3cd10f83c5ead2400b5de472392a7e
sgp-project <d24ce166@charusat.edu.in>
2024-07-28 19:19:52 +0530

    page added

---
+++ aboutus.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="logo">
+			<img src="logo.png" alt="Logo">
+		</div>
+		<div class="name">
+			<h1>Physofit </h1>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="aboutus.php">About Us</a></li>
+				<li><a href="contactus.php">Contact Us</a></li>
+				
+                <?php   
+                session_start();
+                if(isset($_SESSION['stat']))
+                {?>
+                    <li><a href="profile.php">Profile</a></li><?php
+                }
+                else{?>
+                    <li><a href="loginsignup.php">Login/Sign Up</a></li>
+                    <?php
+                }
+                ?>
+				
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
