850adb03a4354dda51cbf76d589e5d6a8093048a
samarth-bhalala <d24ce166@charusat.edu.in>
2024-07-30 16:32:47 +0530

    style.css

--- index.php
+++ index.php
@@ -3,17 +3,107 @@
 <html>
 <head>
 	<title>index</title>
-	<link rel="stylesheet" href="style.css">
+	<style>
+	
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  
+  body {
+    font-family: Arial, sans-serif;
+    line-height: 1.6;
+  }
+  
+  
+  header {
+    background: #333;
+    color: #fff;
+    padding: 10px 0;
+  }
+  
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+  }
+  
+  .logo img {
+    max-height: 50px;
+  }
+  
+  .name h1 {
+    margin: 0;
+    font-size: 24px;
+  }
+  
+  ul {
+    list-style: none;
+    display: flex;
+  }
+  
+  ul li {
+    margin-left: 20px;
+  }
+  
+  ul li a {
+    color: #fff;
+    text-decoration: none;
+    font-size: 16px;
+  }
+  
+  ul li a:hover {
+    text-decoration: underline;
+  }
+  
+
+  @media (max-width: 768px) {
+    .name h1 {
+      font-size: 20px;
+    }
+  
+    ul {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+  
+    ul li {
+      margin-left: 0;
+      margin-bottom: 10px;
+    }
+  }
+  
+
+  @media (max-width: 480px) {
+    .logo img {
+      max-height: 40px;
+    }
+  
+    .name h1 {
+      font-size: 18px;
+    }
+  
+    ul li a {
+      font-size: 14px;
+    }
+  }
+  
+		</style>
+	<!-- <link rel="stylesheet" href="style.css"> -->
 </head>
 <body>
 	<header>
+		<nav>
 		<div class="logo">
 			<img src="img/LOGO.png" alt="Logo">
 		</div>
 		<div class="name">
-			<h1>Physofit</h1>
+			<h1>PHYSIOFIT</h1>
 		</div>
-		<nav>
+		
 			<ul>
 				<li><a href="index.php">Home</a></li>
 				<li><a href="aboutus.php">About Us</a></li>
