ca1c3776553578195b6b5562b5edee6ab0d04c9a
samarth-bhalala <d24ce166@charusat.edu.in>
2024-08-03 16:27:32 +0530

    nav

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +8,7 @@
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+   
   }
   
   
@@ -19,44 +19,58 @@
   
   
   header {
-    background: #333;
-    color: #fff;
-    padding: 10px 0;
+    background: #FFF8F1;
+    color: #333;
+    /* padding: px 0; */
+    margin: 0px 0px 0px 0px;
   }
   
   nav {
+    background: #FFF8F1;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0 20px;
+    color: #333;
+    margin: 0px 0px 0px 0px;
   }
   
   .logo img {
-    max-height: 50px;
+    max-height: 100px;
+    margin: 0px 0px 0px 0px;
   }
   
   .name h1 {
     margin: 0;
     font-size: 24px;
+    color: #333;
   }
   
   ul {
     list-style: none;
     display: flex;
+    color: #333;
+    text-decoration: none;
   }
   
   ul li {
     margin-left: 20px;
+    color: #333;
+    text-decoration: none;
   }
   
-  ul li a {
-    color: #fff;
+  .nav-link {
     text-decoration: none;
     font-size: 16px;
+    color: #333;
+    padding: 10px 20px;
+    border-radius: 10px;
+    transition: background-color 0.3s ease;
   }
   
-  ul li a:hover {
-    text-decoration: underline;
+  .nav-link:hover {
+    background-color: #ffcc00;
+    color: #fff;
   }
   
 
@@ -79,14 +93,15 @@
 
   @media (max-width: 480px) {
     .logo img {
-      max-height: 40px;
+      max-height: 100px;
+      margin: 0px 0px 0px 0px;
     }
   
     .name h1 {
       font-size: 18px;
     }
   
-    ul li a {
+    .nav-link {
       font-size: 14px;
     }
   }
@@ -98,25 +113,25 @@
 	<header>
 		<nav>
 		<div class="logo">
-			<img src="img/LOGO.png" alt="Logo">
+			<img src="img/LOGO1.png" alt="Logo">
 		</div>
 		<div class="name">
 			<h1>PHYSIOFIT</h1>
 		</div>
 		
 			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="aboutus.php">About Us</a></li>
-				<li><a href="contactus.php">Contact Us</a></li>
+				<li><a class="nav-link" href="index.php">Home</a></li>
+				<li><a class="nav-link" href="aboutus.php">About Us</a></li>
+				<li><a class="nav-link" href="contactus.php">Contact Us</a></li>
 				
                 <?php   
                 session_start();
                 if(isset($_SESSION['stat']))
                 {?>
-                    <li><a href="profile.php">Profile</a></li><?php
+                    <li><a class="nav-link" href="profile.php">Profile</a></li><?php
                 }
                 else{?>
-                    <li><a href="loginsignup.php">Login/Sign Up</a></li>
+                    <li><a class="nav-link" href="loginsignup.php">Login/Sign Up</a></li>
                     <?php
                 }
                 ?>
