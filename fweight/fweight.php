d809cd10be9c687f904a6676551be685a9b81c9f
samarthbhalala <d24ce166@charusat.edu.in>
2024-09-08 15:36:08 +0530

    Revert "1"
    This reverts commit 1cad1a1cef256cf3e022d798530b9f49ab1da546.

--- fweight/fweight.php
+++ fweight/fweight.php
@@ -226,7 +226,7 @@ session_start();
     <header>
         <nav>
             <div class="logo">
-                <img src="/sgpproject/sgpproject/img/LOGO.PNG" alt="Logo">
+                <img src="/sgpproject/sgpproject/img/LOGO_1.PNG" alt="Logo">
             </div>
             <div class="name">
                 <h1>PhysioFit</h1>
--- weight.php
+++ weight.php
@@ -200,7 +200,7 @@ main {
     <header>
         <nav>
             <div class="logo">
-                <img src="img/LOGO.PNG" alt="Logo">
+                <img src="img/LOGO_1.PNG" alt="Logo">
             </div>
             <div class="name">
                 <h1>PhysioFit</h1>
