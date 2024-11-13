349b9ef72bfa4a9fe0db0e3a4ccbfdfb847c4bc9
samarthbhalala <175678539+samarth-bhalala@users.noreply.github.com>
2024-11-13 12:13:33 +0530

    delete all files

--- conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con= mysqli_connect("localhost","root","","physio");
-if (!$con)
-{
-    die('Could not connect: ' . mysqli_error());
-}
-?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-$_SESSION['stat']=0;
-session_destroy();
-header("Location:index.php")
-?>
